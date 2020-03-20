8da8467796c50d31fbe7930af5cdf47f217bab0b
jonmeidell <jonmeidell@gmail.com>
2020-03-19 22:37:39 -0600

    clean-up, fixes to javascript and index, minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,43 @@ var lettersGuessed = [];
 var guessesLeft = 12;
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var directionsText = document.getElementById("directions-text");
-var userText = document.getElementById("userchoice-text");
+var letters = document.getElementById("letters-guessed");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var emptyArray = document.getElementById("empty-array");
+var currentWord = document.getElementById("empty-array");
 
 var words = ["belmont", "dracula", "alucard", "death", "medusa", "trevor", "simon", "carmilla", "sypha",
     "grant", "frankenstein", "slogra", "gaibon", "golem", "legion", "mummy", "hector", "isaac"];
 var word = words[Math.floor(Math.random() * words.length)];
 var lettersLeft = word.length;
 var emptyArray = [];
+
 function makeEmptyArray(arr) {
-
-
     for (var i = 0; i < arr.length; i++) {
         emptyArray.push('_');
-        // emptySpot.textContent("");
     }
-
-    return emptyArray;
+    currentWord.textContent = emptyArray.join(" ");
 }
+
 makeEmptyArray(word);
 
 console.log(word);
 console.log(emptyArray);
 
-
-
 function gameState() {
-    if (!emptyArray.includes("_") && guessesLeft >= 0) {
+    if (!emptyArray.includes("_") && guessesLeft >= 1) {
         console.log("You've defeated Dracula");
-        wins + 1;
+        wins++;
+        winsText.innerHTML = "Wins: " + wins;
+        document.getElementById("picture").setAttribute("src", "assets/images/" + word + ".jpg");
+        newWord();
 
-    } else if (guessesLeft <= -1) {
+    } else if (guessesLeft <= 0) {
         console.log("Dracula wins!");
-        losses += 1;
-    } 
+        losses++;
+        lossesText.innerHTML = "Losses: " + losses;
+        newWord();
+    }
 }
 
 function newWord() {
@@ -49,43 +50,33 @@ function newWord() {
     guessesLeft = 12;
     makeEmptyArray(word);
     document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft;
+    document.getElementById("empty-array").innerHTML;
+    letters.innerHTML = "Guesses: ";
 }
 
 document.onkeyup = function (event) {
+    if (alphabet.includes(event.key) && !lettersGuessed.includes(event.key)) {
+        lettersGuessed.push(event.key);
+        letters.innerHTML = lettersGuessed;
 
-    if (guessesLeft > 0) {
-
-        if (alphabet.includes(event.key) && !lettersGuessed.includes(event.key)) {
-            lettersGuessed.push(event.key);
-            guessesLeft -= 1;
-            document.getElementById("wins-text").innerHTML = "Wins: " + wins;
-            document.getElementById("losses-text").innerHTML = "Losses: " + losses;
-            document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft;
-
-            console.log(event.key);
-
-                var indexes = [];
-                for (var i = 0; i < word.length; i++) {
-                    if (word[i] === event.key) {
-                        indexes.push(i)
-                    }
+        if (word.includes(event.key)) {
+            for (var i = 0; i < word.length; i++) {
+                if (word[i] === event.key) {
+                    emptyArray[i] = word[i];
                 }
-                indexes.forEach(function (item) {
-                    emptyArray.splice(item, 1, event.key);
-                })
-                gameState();
-                console.log(emptyArray);
-                console.log(indexes);
-                console.log(guessesLeft);
+            }
+            currentWord.textContent = emptyArray.join(" ");
 
-        } else if (lettersGuessed.includes(event.key)) {
-            console.log("You've already guessed " + event.key)
-            gameState();
         } else {
-            console.log(word + " does not contain " + event.key);
-
-            gameState();
-        
+            guessesLeft -= 1;
+            document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft
         }
+
+        gameState();
+
+    } else if (lettersGuessed.includes(event.key)) {
+        console.log("You've already guessed " + event.key);
+    } else {
+        console.log("Please enter a valid key a-z.");
     }
 }
